2c79dadb3618c888fe03edc9840c00bd7e1f1836
Tim Albert <talbert1209@gmail.com>
2018-01-12 15:13:21 -0600

    Finished project

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,4 +9,17 @@ $(document).ready(function(){
     pauseOnHover: false,
     speed: 700
   });
+
+  var userScroll = 0;
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - userScroll > 50) {
+      var navBarHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navBarHeight}, 150);
+      userScroll = scrollTop;
+    } else if (userScroll - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      userScroll = scrollTop;
+    }
+  });
 });
